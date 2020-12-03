eb72b9587442ccf81126534c9f7eaefc973a2863
MarcosEspinosa28 <maespinosa56@misena.edu.co>
2020-12-03 18:46:32 -0500

    Upgrade Crud Rol Whit Template

--- resources/views/rol/index.blade.php
+++ resources/views/rol/index.blade.php
@@ -1,19 +1,10 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0. maximun-scale=1.0 minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>TIENDA VIRTUAL MT</title>
-</head>
-<body>
+@extends('layouts.admin.app');
+@section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-12">
             <a class="btn btn-primary mt-3 mb-4" href="{{ route('rol.create') }}">Crear Nuevo Rol</a>
-              <table class="table table-striped">
+              <table class="table table-striped mt -30 mb 4 ">
                 <thead>
                 <tr>
                 <th scope="col">ID</th>
@@ -44,5 +35,4 @@
         </div>
     </div>
 </div>
-</body>
-</html>
+@endsection
--- resources/views/rol/show.blade.php
+++ resources/views/rol/show.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0. maximun-scale=1.0 minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>TIENDA VIRTUAL</title>
-</head>
-<body>
+@extends('layouts.admin.app');
+@section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-6 mt-4">
@@ -32,5 +23,5 @@
         </div>
     </div>
 </div>
-</body>
-</html>
+@endsection
+
