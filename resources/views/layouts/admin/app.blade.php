b7bb9170a80e95c7501fac520c46a025269d6cac
AndresFelipeOspinaC <afospina93@misena.edu.co>
2021-04-17 17:55:18 -0500

    ajustes admin relleno

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -7,14 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Standard Buttons - Wide selection of buttons that feature different styles for backgrounds, borders and hover options!</title>
+    <title> Admin-Tienda MT</title>
     <meta name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="Wide selection of buttons that feature different styles for backgrounds, borders and hover options!">
     <meta name="msapplication-tap-highlight" content="no">
     <link rel="stylesheet" href="{{url('css/app.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
- 
+
 </head>
 
 <body>
@@ -29,62 +29,7 @@
       @include('layouts.admin.components.sidebar')
         <div class="app-main__outer">
             <div class="app-main__inner">
-                <!-- Migas de pan-->
-                <div class="app-page-title">
-                    <div class="page-title-wrapper">
-                        <div class="page-title-heading">
-                            <div class="page-title-icon">
-                                <i class="pe-7s-plane icon-gradient bg-tempting-azure"></i>
-                            </div>
-                            <div>Standard Buttons
-                                <div class="page-title-subheading">Wide selection of buttons that feature different styles for backgrounds, borders and hover options!</div>
-                            </div>
-                        </div>
-                        <div class="page-title-actions">
-                            <button type="button" data-toggle="tooltip" title="Example Tooltip" data-placement="bottom"
-                                    class="btn-shadow mr-3 btn btn-dark">
-                                <i class="fa fa-star"></i>
-                            </button>
-                            <div class="d-inline-block dropdown">
-                                <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="btn-shadow dropdown-toggle btn btn-info">
-                                            <span class="btn-icon-wrapper pr-2 opacity-7">
-                                                <i class="fa fa-business-time fa-w-20"></i>
-                                            </span>
-                                    Buttons
-                                </button>
-                                <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
-                                    <ul class="nav flex-column">
-                                        <li class="nav-item">
-                                            <a class="nav-link">
-                                                <i class="nav-link-icon lnr-inbox"></i>
-                                                <span> Inbox</span>
-                                                <div class="ml-auto badge badge-pill badge-secondary">86</div>
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link">
-                                                <i class="nav-link-icon lnr-book"></i>
-                                                <span> Book</span>
-                                                <div class="ml-auto badge badge-pill badge-danger">5</div>
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link">
-                                                <i class="nav-link-icon lnr-picture"></i>
-                                                <span> Picture</span>
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a disabled class="nav-link disabled">
-                                                <i class="nav-link-icon lnr-file-empty"></i>
-                                                <span> File Disabled</span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>    </div>
-                </div>
+
 
                 @yield('content')
             </div>
