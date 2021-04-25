c097810571e7989b50e32240548f5113e848ff75
AndresFelipeOspinaC <afospina93@misena.edu.co>
2021-04-25 11:45:26 -0500

    ajustes front login

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -14,7 +14,7 @@
     <meta name="msapplication-tap-highlight" content="no">
     <link rel="stylesheet" href="{{url('css/app.css')}}">
     <link rel="icon" href="assets/img/logomt.PNG"/>
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/styles3.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 
 </head>
