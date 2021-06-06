b3f1f7890025c4ec31080aec2b252cce75bbc9b9
AndresFelipeOspinaC <afospina93@misena.edu.co>
2021-06-06 10:56:25 -0500

    formulario detalles

--- resources/views/productodos/show.blade.php
+++ resources/views/productodos/show.blade.php
@@ -1,49 +1,59 @@
 @extends('layouts.landing.app2')
 @section('content')
 
-    <table class="mt-5">
 
-        <tr>
-            <th scope="col">ID</th>
-            <td>{{$producto->id}}</td>
-        </tr>
-        <tr>
-            <th scope="col">Nombre</th>
-            <td>{{$producto->nombre_producto }}</td>
-        </tr>
-        <tr>
-            <th scope="col">Imagén</th>
-            <td><img src="{{asset('storage').'/'. $producto->imagen}}" alt="{{$producto->nombre_producto}}" width="200px" height="100px"></td>
-        </tr>
-        <tr>
-            <th scope="col">Talla</th>
-            <td>{{$producto->Talla->talla }}</td>
-        </tr>
-        <tr>
-            <th scope="col">Color</th>
-            <td>{{$producto->Color->color }}</td>
-        </tr>
-        <tr>
-            <th scope="col">Material</th>
-            <td>{{$producto->Material->material }}</td>
-        </tr>
-        <tr>
-            <th scope="col">Categoria</th>
-            <td>{{$producto->Categoria->Nombre }}</td>
-        </tr>
-        <tr>
-            <th scope="col">Clasificación</th>
-            <td>{{$producto->Clasificacion->Nombre }}</td>
-        </tr>
-        <tr>
-            <th scope="col">Precio</th>
-            <td>{{$producto->valor }}</td>
-        </tr>
-        <tr>
-            <th scope="col">Cantidad</th>
-            <td>{{$producto->cantidad }}</td>
-        </tr>
 
-    </table>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-6 mt-4">
+                <div class="table-responsive">
+                    <table class="table ">
+                        <tr>
+                            <th scope="col">ID</th>
+                            <td>{{$producto->id}}</td>
+                        </tr>
+                        <tr>
+                            <th scope="col">Nombre</th>
+                            <td>{{$producto->nombre_producto }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="col">Imagén</th>
+                            <td><img src="{{asset('storage').'/'. $producto->imagen}}" alt="{{$producto->nombre_producto}}" width="200px" height="100px"></td>
+                        </tr>
+                        <tr>
+                            <th scope="col">Talla</th>
+                            <td>{{$producto->Talla->talla }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="col">Color</th>
+                            <td>{{$producto->Color->color }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="col">Material</th>
+                            <td>{{$producto->Material->material }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="col">Categoria</th>
+                            <td>{{$producto->Categoria->Nombre }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="col">Clasificación</th>
+                            <td>{{$producto->Clasificacion->Nombre }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="col">Precio</th>
+                            <td>{{$producto->valor }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="col">Cantidad</th>
+                            <td>{{$producto->cantidad }}</td>
+                        </tr>
+
+                    </table>
+                    <a class="btn btn-primary btn-sm" href="{{route('producto.index') }}">Volver</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
