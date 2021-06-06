338ee1218af7fa489ff4b70526326e9606aada8f
Sebastian <jsruiz7411@misena.edu.co>
2021-06-06 15:53:43 -0500

    Cambios

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -18,7 +18,7 @@ class Producto extends Model
     protected $fillable = [
 
         'nombre_producto', 'imagen', 'talla_id', 'color', 'material_id', 'categoria_id', 'clasificacion_id',
-        'precio', 'cantidad','created_at','updated_at'];
+        'valor', 'cantidad','created_at','updated_at'];
 
     public function Clasificacion()
     {
