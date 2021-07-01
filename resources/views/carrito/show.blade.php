db8ccc988c08da0479b8288cc6b8a0b0d68fef91
Sebastian <jsruiz7411@misena.edu.co>
2021-07-01 11:49:44 -0500

    Formato Dinero

--- resources/views/carrito/show.blade.php
+++ resources/views/carrito/show.blade.php
@@ -82,7 +82,7 @@
                             
                 @endforeach
 
-                <p>${{$total}}</p>
+                <p>${{umber_format($total)}}</p>
                
                @if($total != 0)
                 <button type="submit" class="btn btn-success btn-lg mt-4">Pagar</button>
